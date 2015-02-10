3f656ff1d49de4cc007463f93c830bae62e3ef8d
mnasiatka <mnasiatka3@gatech.edu>
2015-02-10 13:33:17 -0500

    Added list view

---
+++ ShopWithFriends/FriendsListViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriendsListViewController.h
+//  ShopWithFriends
+//
+//  Created by Matthew Nasiatka on 2/10/15.
+//  Copyright (c) 2015 Georgia Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendsListViewController : UIViewController
+
+@end
